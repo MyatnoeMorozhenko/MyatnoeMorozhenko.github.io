90ad0d1c7417f1aa84bc06c3b4dfdc7a20c1bf04
Alena <111291767+MyatnoeMorozhenko@users.noreply.github.com>
2024-08-20 01:02:32 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram import types 
 
-keyboard = InlineKeyboardButton('Digital Breakfast', web_app=WebAppInfo(url = 'https://myatnoemorozhenko.github.io/'))
-
+kb = InlineKeyboardButton('Digital Breakfast', web_app=WebAppInfo(url = 'https://myatnoemorozhenko.github.io/'))
+keyboard = InlineKeyboardMarkup().add(kb)
 
