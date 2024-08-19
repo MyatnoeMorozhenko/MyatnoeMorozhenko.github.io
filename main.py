e250aa082a9f632e6d60e9828f16b0891a2d1397
Alena <111291767+MyatnoeMorozhenko@users.noreply.github.com>
2024-08-19 18:51:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
-@dp.message_handler(commands=['start'])
-async def start(msg: types.Message):
-    user_id = msg.from_user.id
-    username = msg.from_user.username
-    await bot.send_photo(msg.from_user.id, caption = f"Добрый день, {msg.from_user.full_name}\!\n\nНажимай на кнопку и смотри меню Digital завтрака!",
-                         parse_mode='MarkdownV2'
-                         reply_markup=keyb_client)
+async def main():
+    from handlers import dp
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.session.close()
 
 if __name__ == "__main__":
     try:
-        executor.start_polling(dp, skip_updates=True)
+        asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         print('Bot stopped!')
