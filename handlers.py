2c2f86f1e493f4304aaa712b2aee7a00b7313886
Alena <111291767+MyatnoeMorozhenko@users.noreply.github.com>
2024-08-19 21:20:15 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -8,15 +8,15 @@ async def start(message: types.Message):
     await bot.send_message(message.chat.id, 'Жми на кнопку и выбирай свой digital breakfast',
                            reply_markup=keyboard)
 
-pay_token = '381764678:TEST:92864'
+pay_token = '1744374395:TEST:c172bfd1a3258f663519'
 
 PRICE = {
     '1': [types.LabeledPrice(label='Item1', amount=100000)],
-    '2': [types.LabeledPrice(label='Item2', amount=200000)],
-    '3': [types.LabeledPrice(label='Item3', amount=300000)],
-    '4': [types.LabeledPrice(label='Item4', amount=400000)],
-    '5': [types.LabeledPrice(label='Item5', amount=500000)],
-    '6': [types.LabeledPrice(label='Item6', amount=600000)]
+    '2': [types.LabeledPrice(label='Item2', amount=70000)],
+    '3': [types.LabeledPrice(label='Item3', amount=65000)],
+    '4': [types.LabeledPrice(label='Item4', amount=20000)],
+    '5': [types.LabeledPrice(label='Item5', amount=20000)],
+    '6': [types.LabeledPrice(label='Item6', amount=15000)]
 }
 
 @dp.message_handler(content_types='web_app_data')
