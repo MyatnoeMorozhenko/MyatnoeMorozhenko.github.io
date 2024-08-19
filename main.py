f6181cc95d5b6fbc245b02c2c06873902e962dd9
Alena <111291767+MyatnoeMorozhenko@users.noreply.github.com>
2024-08-19 18:46:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from aiogram.utils import executor
 import asyncio
 import os
 from dotenv import load_dotenv
+from handlers import dp
 
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
-async def main():
-    from handlers import dp
-    try:
-        await dp.start_polling()
-    finally:
-        await bot.session.close()
+@dp.message_handler(commands=['start'])
+async def start(msg: types.Message):
+    user_id = msg.from_user.id
+    username = msg.from_user.username
+    await bot.send_photo(msg.from_user.id, caption = f"Добрый день, {msg.from_user.full_name}\!\n\nНажимай на кнопку и смотри меню Digital завтрака!",
+                         parse_mode='MarkdownV2'
+                         reply_markup=keyb_client)
 
 if __name__ == "__main__":
     try:
