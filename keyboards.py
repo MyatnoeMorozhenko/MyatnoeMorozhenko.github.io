abedeae63a89aeb0878635891ea1d5db3432da82
Alena <111291767+MyatnoeMorozhenko@users.noreply.github.com>
2024-08-20 00:44:10 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,12 +1,6 @@
-from aiogram.types import WebAppInfo
-from aiogram import types
+from aiogram.types import WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram import types 
 
-web_app = WebAppInfo(url='https://myatnoemorozhenko.github.io/')
+keyboard = InlineKeyboardButton('Digital Breakfast', web_app=WebAppInfo(url = 'https://myatnoemorozhenko.github.io/'))
 
-keyboard = types.ReplyKeyboardMarkup(
-    keyboard=[
-        [types.KeyboardButton(text='Site', web_app=web_app)]
-    ],
-    resize_keyboard=True
-)
 
